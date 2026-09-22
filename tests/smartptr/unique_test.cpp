5346e14def78eb9e91067d3ea613d84e3bdaeae0
qubelk <qubel2221545ga@gmail.com>
2026-09-22 16:24:29 +0300

    Write tests for all Unique constructors

--- tests/smartptr/unique_test.cpp
+++ tests/smartptr/unique_test.cpp
@@ -9,3 +9,29 @@ TEST(UniqueTest, DefaultConstructor) {
 	EXPECT_EQ(u, nullptr);
 	EXPECT_TRUE(u == nullptr);
 }
+
+TEST(UniqueTest, MakeConstructor) {
+	Unique<int> u{42};
+
+	EXPECT_EQ(*u, 42);
+}
+
+TEST(UniqueTest, PointerConstructor) {
+	int* ptr = new int;
+	*ptr = 42;
+
+	Unique<int> u1{ptr};
+
+	EXPECT_EQ(*u1, 42);
+	EXPECT_EQ(ptr, nullptr);
+
+	delete ptr;
+}
+
+TEST(UniqueTest, MoveConstructor) {
+	Unique<int> u1{42};
+	Unique<int> u2{std::move(u1)};
+
+	EXPECT_EQ(*u2, 42);
+	EXPECT_EQ(u1, nullptr);
+}
